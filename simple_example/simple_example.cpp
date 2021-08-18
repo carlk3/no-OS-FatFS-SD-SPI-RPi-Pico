4fce59ae98731ea6d778d8f858bbac85e7f980df
Carl John Kugler III <carlk3@gmail.com>
2021-08-18 16:16:45 -0600

    Update simple_example for changes to sd_card_t

--- simple_example/simple_example.cpp
+++ simple_example/simple_example.cpp
@@ -1,9 +1,11 @@
 #include <stdio.h>
-#include "pico/stdlib.h"
-
-#include "ff.h"
+//
 #include "f_util.h"
+#include "ff.h"
+#include "pico/stdlib.h"
 #include "rtc.h"
+//
+#include "hw_config.h"
 
 int main() {
     stdio_init_all();
@@ -11,14 +13,16 @@ int main() {
 
     puts("Hello, world!");
 
-    // See FatFs - Generic FAT Filesystem Module, "Application Interface", http://elm-chan.org/fsw/ff/00index_e.html
-    static FATFS fatfs;
-    FRESULT fr = f_mount(&fatfs, "", 1); 
+    // See FatFs - Generic FAT Filesystem Module, "Application Interface",
+    // http://elm-chan.org/fsw/ff/00index_e.html
+    sd_card_t *pSD = sd_get_by_num(0);
+    FRESULT fr = f_mount(&pSD->fatfs, pSD->pcName, 1);
     if (FR_OK != fr) panic("f_mount error: %s (%d)\n", FRESULT_str(fr), fr);
     FIL fil;
-    const char * const filename = "filename.txt";
+    const char* const filename = "filename.txt";
     fr = f_open(&fil, filename, FA_OPEN_APPEND | FA_WRITE);
-    if (FR_OK != fr && FR_EXIST != fr) panic("f_open(%s) error: %s (%d)\n", filename, FRESULT_str(fr), fr);
+    if (FR_OK != fr && FR_EXIST != fr)
+        panic("f_open(%s) error: %s (%d)\n", filename, FRESULT_str(fr), fr);
     if (f_printf(&fil, "Hello, world!\n") < 0) {
         printf("f_printf failed\n");
     }
@@ -26,7 +30,7 @@ int main() {
     if (FR_OK != fr) {
         printf("f_close error: %s (%d)\n", FRESULT_str(fr), fr);
     }
-    f_unmount("");
+    f_unmount(pSD->pcName);
 
     puts("Goodbye, world!");
     for (;;);
