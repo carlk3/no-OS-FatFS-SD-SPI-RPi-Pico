1a745d132c3c6b78e5fb8eeedacb46d90045ac91
Bernhard Bablok <bablokb@gmx.de>
2022-01-31 19:10:44 +0100

    removed unneeded lines

--- example/maker_pi_pico.hw_config.c
+++ example/maker_pi_pico.hw_config.c
@@ -65,13 +65,8 @@ static sd_card_t sd_cards[] = {  // One for each SD card
         .pcName = "0:",           // Name used to mount device
         .spi = &spis[0],          // Pointer to the SPI driving this card
         .ss_gpio = 15,            // The SPI slave select GPIO for this SD card
-        .card_detect_gpio = 22,   // Card detect
-        .card_detected_true = -1,  // What the GPIO read returns when a card is
-                                  // present. Use -1 if there is no card detect.
         // Following attributes are dynamically assigned
         .m_Status = STA_NOINIT,
-        .sectors = 0,
-        .card_type = 0,
     }
 };
 
